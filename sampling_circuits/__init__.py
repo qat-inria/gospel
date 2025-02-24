fef1456a72785c56af72603b284e4081f099c01d
Thierry Martinez <thierry.martinez@inria.fr>
2025-02-24 22:26:34 +0100

    Fix GitHub actions

---
+++ sampling_circuits/__init__.py
@@ -0,0 +1,13 @@
+from sampling_circuits.sample_circuits import (
+    estimate_circuits,
+    generate_circuits,
+    sample_circuit,
+    sample_circuits,
+)
+
+__all__ = [
+    "estimate_circuits",
+    "generate_circuits",
+    "sample_circuit",
+    "sample_circuits",
+]
