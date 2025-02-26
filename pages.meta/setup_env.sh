cc355cb416b77399353b0c4b6cb682400343f55a
Thierry Martinez <thierry.martinez@inria.fr>
2025-02-27 00:38:04 +0100

    Add rsync in env

--- pages.meta/setup_env.sh
+++ pages.meta/setup_env.sh
@@ -1,2 +1,4 @@
 set -ex
-sudo apt-get install --yes curl less liblttng-ust1t64 libssl-dev libicu-dev
+sudo apt-get install --yes less rsync
+# For GitHub Actions
+sudo apt-get install --yes curl liblttng-ust1t64 libssl-dev libicu-dev
