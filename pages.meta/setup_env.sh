a70d7aabfd7b201568df7ed30c4b9a6dac2ff26e
Thierry Martinez <thierry.martinez@inria.fr>
2025-02-27 00:49:03 +0100

    Install git

--- pages.meta/setup_env.sh
+++ pages.meta/setup_env.sh
@@ -1,4 +1,4 @@
 set -ex
-sudo apt-get install --yes less rsync
+sudo apt-get install --yes less rsync git
 # For GitHub Actions
 sudo apt-get install --yes curl liblttng-ust1t64 libssl-dev libicu-dev
