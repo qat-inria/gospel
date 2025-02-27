d83a7f685a9c01a80ed2d8813b2740c86edbd4d1
Thierry Martinez <thierry.martinez@inria.fr>
2025-02-27 11:35:57 +0100

    Build list so that tqdm can estimate progression

--- scripts/qasm2img.py
+++ scripts/qasm2img.py
@@ -16,7 +16,7 @@ def convert_circuit_directory() -> None:
     circuits_path = Path("circuits")
     circuits_svg_path = Path("circuits_svg")
     circuits_svg_path.mkdir()
-    for circuit in tqdm(circuits_path.glob("*.qasm")):
+    for circuit in tqdm(list(circuits_path.glob("*.qasm"))):
         qasm2img(circuit, (circuits_svg_path / circuit.name).with_suffix(".svg"))
 
 
