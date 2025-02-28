12e93d884424c1da6bf651314fdd21dc9e3a27d3
mgarnier59 <50111289+mgarnier59@users.noreply.github.com>
2025-02-28 15:31:09 +0100

    add colormap with example in the notebook

--- gospel/scripts/qasm2brickwork_state.py
+++ gospel/scripts/qasm2brickwork_state.py
@@ -121,12 +121,57 @@ def draw_brickwork_state(circuit: Circuit, target: Path) -> None:
     plt.close()
 
 
+def draw_brickwork_state_colormap(
+    circuit: Circuit, target: Path, failure_probas: dict[int, float]
+) -> None:
+    """Draw the brickwork state with trap failure probability drawn as the node color.
+    Heavily redundant since we have already gone through the transpilation step.
+
+    Parameters
+    ----------
+    circuit : Circuit
+
+    target : Path
+        where to save the figure
+    failure_probas : dict[int, float]
+        dictionary of failure probability (value) by node (key)
+    """
+
+    pattern = gospel.brickwork_state_transpiler.transpile(circuit)
+    graph = OpenGraph.from_pattern(pattern)
+    pos = gospel.brickwork_state_transpiler.get_node_positions(
+        pattern, reverse_qubit_order=True
+    )
+    labels = {node: node for node in graph.inside.nodes()}
+    colors = [failure_probas[node] for node in graph.inside.nodes()]
+
+    plt.figure(
+        figsize=(max(x for x, y in pos.values()), max(y for x, y in pos.values()))
+    )
+    nx.draw_networkx_edges(graph.inside, pos, edge_color="black")
+    nc = nx.draw_networkx_nodes(
+        graph.inside,
+        pos,
+        nodelist=graph.inside.nodes,
+        label=labels,
+        node_color=colors,
+        node_size=1000,
+        cmap=plt.cm.jet,
+        vmin=0,
+        vmax=1,
+    )
+    plt.colorbar(nc)
+    plt.axis("off")
+    plt.savefig(target, format="svg")
+    plt.close()
+
+
 def convert_circuit_directory_to_brickwork_state(
     path_circuits: Path, path_brickwork_state_svg: Path
 ) -> None:
     path_brickwork_state_svg.mkdir()
     for path_circuit in tqdm(list(path_circuits.glob("*.qasm"))):
-        with Path(path_circuit).open() as f:
+        with Path(path_circuits).open() as f:
             circuit = read_qasm(f)
             target = (path_brickwork_state_svg / path_circuit.name).with_suffix(".svg")
             draw_brickwork_state(circuit, target)
