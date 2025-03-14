5538b5686c6182302958b7335fc9126fc4a3c2ec
Thierry Martinez <thierry.martinez@inria.fr>
2025-03-15 00:31:51 +0100

    Larger parameters

--- gospel/cluster/run_veriphix.py
+++ gospel/cluster/run_veriphix.py
@@ -162,12 +162,12 @@ def run() -> None:
         cluster = SLURMCluster(
             account="inria",
             queue="cpu_devel",
-            cores=1,
+            cores=4,
             memory="1GB",
             walltime="00:30:00",
             scheduler_options={"dashboard_address": f":{portdash}"},
         )
-        cluster.scale(10)
+        cluster.scale(20)
     else:
         cluster = dask.distributed.LocalCluster()
         cluster.scale(5)
