3ef7bc974b4e115b05239d9ccb413e5008ddd282
Sami Abdul Sater <sabdulsa@cleps.localdomain>
2025-03-20 01:15:50 +0100

    Add sim results

--- gospel/cluster/simulations.sh
+++ gospel/cluster/simulations.sh
@@ -7,11 +7,12 @@ n_instances=100
 bqp_error=0.4
 # python -m gospel.cluster.generate_circuit_sample $n_instances $bqp_error
 
-n_comp_run=100
-n_test_run=7
+
+n_comp_run=20
+n_test_run=0
 n_nodes=$n_instances
 
-for p_err in  0.01 0.05 0.1 0.15 0.18 0.22 0.3 0; do
+for p_err in  0.1 0.2 0.3 0.4 0.5 ; do
   PORT=24395
 
   # Print p and assigned port
