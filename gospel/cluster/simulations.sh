c2b7ab04375ff17b3ddb14cd1422d5db9da94a0e
Sami AS <samiabdulsater@gmail.com>
2025-03-21 16:14:25 +0000

    Rename old results for strong global, prepare for new sim

--- gospel/cluster/simulations.sh
+++ gospel/cluster/simulations.sh
@@ -35,7 +35,7 @@ for p_err in 0.01 0.05 0.1 0.15 0.18 0.2 0.3 0.5 0.7 ; do
   echo "Running with p_err=$p_err, PORT=$PORT"
 
   # Run the process in the background
- # time python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes
+  time python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes
 
 done
 
@@ -48,7 +48,7 @@ for p_err in 0.00005 0.000075 0.0001 0.00015 0.000175 0.0002 ; do
   echo "Running with p_err=$p_err, PORT=$PORT"
 
   # Run the process in the background
-#  time python -m gospel.cluster.run_veriphix-depol $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes
+  # time python -m gospel.cluster.run_veriphix-depol $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes
 
 done
 
@@ -61,7 +61,7 @@ for p_err in 0.0025 0.004 0.006 0.0015 ; do
   echo "Running with p_err=$p_err, PORT=$PORT"
 
   # Run the process in the background
-  time python -m gospel.cluster.run_veriphix-uncorr_depol $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes
+  # time python -m gospel.cluster.run_veriphix-uncorr_depol $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes
 
 done
 
