01715fb599694db679fd50c7d8593bb9d89f0d95
h-oll@cleps <harold.ollivier@inria.fr>
2025-03-22 15:28:10 +0100

    STRONG GLOBAL with 100 instances, depth=30 and p_err = .05, .1, .2, .3, .4

--- gospel/cluster/simulations.sh
+++ gospel/cluster/simulations.sh
@@ -3,7 +3,7 @@
 # rm -rf circuits
 # python -m gospel.sampling_circuits.experiments
 
-n_instances=30
+n_instances=100
 bqp_error=0.4
 # python -m gospel.cluster.generate_circuit_sample $n_instances $bqp_error
 
@@ -28,17 +28,18 @@ n_nodes=$n_instances
 
 echo "STRONG GLOBAL NOISE"
 # Strong global noise
-for p_err in 0.5 ; do
+for p_err in 0.05 0.1 0.2 0.3 0.4 ; do
   PORT=24395
 
   # Print p and assigned port
   echo "Running with p_err=$p_err, PORT=$PORT"
 
   # Run the process in the background locally
-  time python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --scale $n_nodes
+  #time python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --scale $n_nodes
 
   # Run the process in the background on the cluster
-  #time python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes
+  nohup python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes 
+
 
 done
 
