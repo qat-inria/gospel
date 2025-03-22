4328e2c90da52e7de84256561fed7ed75cefec4c
holl@potassium-dev <harold.ollivier@inria.fr>
2025-03-22 21:25:24 +0100

    Adding Malicious Noise model

--- gospel/cluster/simulations.sh
+++ gospel/cluster/simulations.sh
@@ -26,22 +26,39 @@ n_nodes=$n_instances
 
 # done
 
-echo "STRONG GLOBAL NOISE"
+# echo "STRONG GLOBAL NOISE"
+# # Strong global noise
+# for p_err in 0.5 ; do
+#   PORT=24395
+
+#   # Print p and assigned port
+#   echo "Running with p_err=$p_err, PORT=$PORT"
+
+#   # Run the process in the background locally
+#   #time python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --scale $n_nodes
+
+#   # Run the process in the background on the cluster
+#   nohup python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes 
+
+# done
+
+echo "MALICIOUS"
 # Strong global noise
-for p_err in 0.5 ; do
+for p_err in 1.000 ; do
   PORT=24395
 
   # Print p and assigned port
   echo "Running with p_err=$p_err, PORT=$PORT"
 
   # Run the process in the background locally
-  #time python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --scale $n_nodes
+  time python -m gospel.cluster.run_veriphix-malicious $n_comp_run $n_test_run $n_instances $p_err $bqp_error --scale 12
 
   # Run the process in the background on the cluster
-  nohup python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes 
-
+  #nohup python -m gospel.cluster.run_veriphix-strong $n_comp_run $n_test_run $n_instances $p_err $bqp_error --walltime 3 --memory 4 --cores 4 --port $PORT --scale $n_nodes 
 
 done
+
+
 
 # echo "DEPOLARIZING (CORRELATED)"
 # # Depolarizing
