b9d18213b0d0f3097917f64fea0001e5d1972f2c
holl@cleps <harold.ollivier@inria.fr>
2025-03-22 21:41:11 +0100

    extending parameter sweep for STRONG GLOBAL

--- gospel/cluster/simulations.sh
+++ gospel/cluster/simulations.sh
@@ -28,7 +28,7 @@ n_nodes=$n_instances
 
 echo "STRONG GLOBAL NOISE"
 # Strong global noise
-for p_err in 0.5 ; do
+for p_err in 0.6 0.7 0.8 0.9 1.0 ; do
   PORT=24395
 
   # Print p and assigned port
